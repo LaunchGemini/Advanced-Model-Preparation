b0aa8fca08d4404b36e36ad8ad3f4cba5ab9b077
LaunchGemini <metabaseguy@proton.me>
2022-03-04 20:06:38 +0000

    kthread: Implement park/unpark facility

--- models/classification/_base_/resnet50_omz.py
+++ models/classification/_base_/resnet50_omz.py
@@ -1,13 +1,14 @@
 # model settings
 model = dict(
     type='ImageClassifier',
+    pretrained=None,
     backbone=dict(
-        type='ResNet',
-        depth=50,
-        in_channels=3,
-        num_stages=4,
-        out_indices=(3, ),
-        norm_cfg=dict(type='BN')),
+        type='OmzBackboneCls',
+        mode='train',
+        model_path='public/resnet-50-caffe2/FP32/resnet-50-caffe2.xml',
+        last_layer_name='gpu_0/res5_2_branch2c_bn_3',
+        normalized_img_input=True
+    ),
     neck=dict(
         type='GlobalAveragePooling'),
     head=dict(
