2078fdab074c53d71e91a081abce93665094eb08
LaunchGemini <metabaseguy@proton.me>
2022-03-05 06:47:09 +0000

    ext4: check for overlapping extents in ext4_valid_extent_entries()

---
+++ models/segmentation/_base_/ocr_litehrnet18_mod2.py
@@ -0,0 +1,41 @@
+__norm_cfg = dict(type='BN', requires_grad=True)
+model = dict(
+    type='ClassIncrSegmentor',
+    is_task_adapt=True,
+    pretrained=None,
+    backbone=dict(
+        type='LiteHRNet',
+        norm_cfg=__norm_cfg,
+        norm_eval=False,
+        extra=dict(
+            stem=dict(
+                stem_channels=32,
+                out_channels=32,
+                expand_ratio=1,
+                strides=(2, 2),
+                extra_stride=False,
+                input_norm=False,
+            ),
+            num_stages=3,
+            stages_spec=dict(
+                num_modules=(2, 4, 2),
+                num_branches=(2, 3, 4),
+                num_blocks=(2, 2, 2),
+                module_type=('LITE', 'LITE', 'LITE'),
+                with_fuse=(True, True, True),
+                reduce_ratios=(8, 8, 8),
+                num_channels=(
+                    (40, 80),
+                    (40, 80, 160),
+                    (40, 80, 160, 320),
+                )
+            ),
+            out_modules=dict(
+                conv=dict(
+                    enable=False,
+                    channels=320
+                ),
+                position_att=dict(
+                    enable=False,
+                    key_channels=128,
+                    v
