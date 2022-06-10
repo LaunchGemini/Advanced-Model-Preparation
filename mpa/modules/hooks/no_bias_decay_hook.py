64867f2a0f58afffc13aa6bd7bd379c5e00191c6
LaunchGemini <metabaseguy@proton.me>
2022-06-10 11:14:09 +0000

    linking

--- mpa/modules/hooks/no_bias_decay_hook.py
+++ mpa/modules/hooks/no_bias_decay_hook.py
@@ -38,4 +38,33 @@ class NoBiasDecayHook(Hook):
         weight_decay_group["params"] = weight_decay
 
         bias_group = runner.optimizer.param_groups[0].copy()
-        bias_group[
+        bias_group["params"] = bias_no_decay
+        bias_group["weight_decay"] = 0.0
+        bias_group["lr"] *= 2
+
+        weight_no_decay_group = runner.optimizer.param_groups[0].copy()
+        weight_no_decay_group["params"] = weight_no_decay
+        weight_no_decay_group["weight_decay"] = 0.0
+
+        param_groups = [weight_decay_group, bias_group, weight_no_decay_group]
+        runner.optimizer.param_groups = param_groups
+
+    def after_train_epoch(self, runner):
+        params = []
+        for m in runner.model.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                params.append(m.weight)
+                if m.bias is not None:
+                    params.append(m.bias)
+            elif hasattr(m, "weight") or hasattr(m, "bias"):
+                if hasattr(m, "weight"):
+                    params.append(m.weight)
+                if hasattr(m, "bias"):
+                    params.append(m.bias)
+            elif len(list(m.children())) == 0:
+                for p in m.parameters():
+                    params.append(p)
+
+        param_groups = runner.optimizer.param_groups[0].copy()
+        param_groups["params"] = params
+        runner.optimizer.param_groups = [param_groups]
---
+++ mpa/modules/hooks/sam_optimizer_hook.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import torch
+from mmcv.runner import HOOKS, OptimizerHook
+
+
+@HOOKS.register_module()
+class SAMOptimizerHook(OptimizerHook):
+    '''Sharpness-aware Minimization optimizer hook
+
+    Implemented as OptimizerHook for MMCV Runners
+    - Paper ref: https://arxiv.org/abs/2010.01412
+    - code ref: https://github.com/davda54/sam
+    '''
+
+    def __init__(self, rho=0.05, start_epoch=1, **kwargs):
+        super().__init__(**kwargs)
+        self.rho = rho
+        self.start_epoch = start_epoch
+        if rho < 0.0:
+            raise ValueError('rho should be greater than 0 for SAM optimizer')
+
+    def after_train_iter(self, runner):
+        '''Perform SAM optimization
+        0. compute current loss (DONE IN model.train_step())
+        1. compute current gradient
+        2. move param to the approximate local maximum: w + e(w) = w + rho*norm_grad
+        3. compute maximum loss
+        4. compute SAM gradient on maximum loss
+        5. restore parram to original param
+        6. update param using SAM gradient
+
+        Assuming model.current_batch had been set in model.train_step()
+        '''
+        current_batch = self._get_current_batch(runner.model)
+        if current_batch is None or runner.epoch+1 < self.start_epoch:
+            # Apply original parameter update
+            return super().after_train_iter(runner)
+
+        # Current gradient
+        runner.optimizer.zero_grad()
+        curr_loss = runner.outputs['loss']
+        curr_loss.backward()
+
+        # Move to local maximum
+        param2move = {}
+        with torch.no_grad():
+            scale = self.rho / (self._grad_norm(runner.optimizer) + 1e-12)
+            for param_group in runner.optimizer.param_groups:
+                for param in param_group['params']:
+                    if param.grad is None:
+                        continue
+                    e_w = param.grad * scale.to(param)
+                  
