4f17c3725d5b14df206e6e0142914b43cc0767fc
LaunchGemini <metabaseguy@proton.me>
2022-07-19 13:43:30 +0000

    Initial project import.

--- mpa/modules/models/detectors/unbiased_teacher.py
+++ mpa/modules/models/detectors/unbiased_teacher.py
@@ -125,4 +125,73 @@ class UnbiasedTeacher(SAMDetectorMixin, BaseDetector):
             if isinstance(ul_loss, torch.Tensor):
                 ul_loss = [ul_loss]
             losses[ul_loss_name + '_ul'] = [
-                los
+                loss*self.unlabeled_loss_weight for loss in ul_loss
+            ]
+        # TODO: apply loss_bbox when adopting QFL;
+
+        return losses
+
+    def generate_pseudo_labels(self, teacher_outputs, **kwargs):
+        all_pseudo_bboxes = []
+        all_pseudo_labels = []
+        num_all_bboxes = 0
+        num_all_pseudo = 0
+        if len(teacher_outputs[0][0].shape) == len(teacher_outputs[0][1].shape):
+            teacher_outputs = [(teacher_outputs[0][i], teacher_outputs[1][i]) for i in range(len(teacher_outputs[0]))]
+        for teacher_bboxes, teacher_labels in teacher_outputs:
+            # print(teacher_bboxes.shape, teacher_labels.shape)
+            confidences = teacher_bboxes[:, -1]
+            pseudo_indices = confidences > self.pseudo_conf_thresh
+            pseudo_bboxes = teacher_bboxes[pseudo_indices, :4]  # model output: [x y w h conf]
+            pseudo_labels = teacher_labels[pseudo_indices]
+            all_pseudo_bboxes.append(pseudo_bboxes)
+            all_pseudo_labels.append(pseudo_labels)
+            num_all_bboxes += teacher_bboxes.shape[0]
+            num_all_pseudo += pseudo_bboxes.shape[0]
+        # print(f'{num_all_pseudo} / {num_all_bboxes}')
+        pseudo_ratio = float(num_all_pseudo)/num_all_bboxes if num_all_bboxes > 0 else 0.0
+        return all_pseudo_bboxes, all_pseudo_labels, pseudo_ratio
+
+    def eval_pseudo_label_recall(self, all_pseudo_bboxes, all_gt_bboxes):
+        # For test only
+        from mmdet.core.evaluation.recall import _recalls, bbox_overlaps
+        img_num = len(all_gt_bboxes)
+        if img_num == 0:
+            return torch.Tensor([0.0])
+        all_ious = np.ndarray((img_num,), dtype=object)
+        for i in range(img_num):
+            ps_bboxes = all_pseudo_bboxes[i]
+            gt_bboxes = all_gt_bboxes[i]
+            # prop_num = min(ps_bboxes.shape[0], 100)
+            prop_num = ps_bboxes.shape[0]
+            if gt_bboxes is None or gt_bboxes.shape[0] == 0:
+                ious = np.zeros((0, ps_bboxes.shape[0]), dtype=np.float32)
+            elif ps_bboxes is None or ps_bboxes.shape[0] == 0:
+                ious = np.zeros((gt_bboxes.shape[0], 0), dtype=np.float32)
+            else:
+                ious = bbox_overlaps(
+                    gt_bboxes.detach().cpu().numpy(),
+                    ps_bboxes.detach().cpu().numpy()[:prop_num, :4]
+                )
+            all_ious[i] = ious
+        recall = _recalls(all_ious, np.array([100]), np.array([0.5]))
+        return torch.Tensor(recall)
+
+    @staticmethod
+    def state_dict_hook(module, state_dict, *args, **kwargs):
+        """Redirect teacher model as output state_dict (student as auxilliary)
+        """
+        logger.info('----------------- UnbiasedTeacher.state_dict_hook() called')
+        output = OrderedDict()
+        for k, v in state_dict.items():
+            if 'model_t.' in k:
+                k = k.replace('model_t.', '')
+            output[k] = v
+        return output
+
+    @staticmethod
+    def load_state_dict_pre_hook(module, state_dict, *args, **kwargs):
+        """Redirect input state_dict to teacher model
+        """
+        logger.info('----------------- UnbiasedTeacher.load_state_dict_pre_hook() called')
+        for k in
