e7c3e6954d6188bef701f9d6e6d8721392a24d16
LaunchGemini <metabaseguy@proton.me>
2022-08-30 22:46:39 +0000

    Commit scriptado

--- mpa/modules/utils/distance_utils.py
+++ mpa/modules/utils/distance_utils.py
@@ -130,4 +130,92 @@ def init_dist(args, backend="nccl"):
         return False
 
     else:
-        
+        raise ValueError("Invalid launcher type: {}".format(args.launcher))
+
+
+def get_dist_info():
+    try:
+        # data distributed parallel
+        return dist.get_rank(), dist.get_world_size(), True
+    except Exception:
+        return 0, 1, False
+
+
+def init_dist_pytorch(args, backend="nccl"):
+    args.rank = int(os.environ["LOCAL_RANK"])
+    if 'CUDA_VISIBLE_DEVICES' in os.environ.keys():
+        args.ngpus_per_node = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+    else:
+        args.ngpus_per_node = torch.cuda.device_count()
+    assert args.ngpus_per_node > 0, "CUDA is not supported"
+    args.gpu = args.rank
+    torch.cuda.set_device(args.gpu)
+    dist.init_process_group(backend=backend)
+    args.total_gpus = dist.get_world_size()
+    args.world_size = args.total_gpus
+
+
+def init_dist_slurm(args, backend="nccl"):
+    args.rank = int(os.environ["SLURM_PROCID"])
+    args.world_size = int(os.environ["SLURM_NTASKS"])
+    node_list = os.environ["SLURM_NODELIST"]
+    if 'CUDA_VISIBLE_DEVICES' in os.environ.keys():
+        args.ngpus_per_node = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
+    else:
+        args.ngpus_per_node = torch.cuda.device_count()
+    assert args.ngpus_per_node > 0, "CUDA is not supported"
+    args.gpu = args.rank % args.ngpus_per_node
+    torch.cuda.set_device(args.gpu)
+    addr = subprocess.getoutput(
+        "scontrol show hostname {} | head -n1".format(node_list)
+    )
+    os.environ["MASTER_PORT"] = str(args.tcp_port)
+    os.environ["MASTER_ADDR"] = addr
+    os.environ["WORLD_SIZE"] = str(args.world_size)
+    os.environ["RANK"] = str(args.rank)
+    dist.init_process_group(backend=backend)
+    args.total_gpus = dist.get_world_size()
+
+
+def simple_group_split(world_size, rank, num_groups):
+    groups = []
+    rank_list = np.split(np.arange(world_size), num_groups)
+    rank_list = [list(map(int, x)) for x in rank_list]
+    for i in range(num_groups):
+        groups.append(dist.new_group(rank_list[i]))
+    group_size = world_size // num_groups
+    print(
+        "Rank no.{} start sync BN on the process group of {}".format(
+            rank, rank_list[rank // group_size]
+        )
+    )
+    return groups[rank // group_size]
+
+
+def convert_sync_bn(model, process_group=None):
+    for _, (child_name, child) in enumerate(model.named_children()):
+        if isinstance(child, nn.modules.batchnorm._BatchNorm):
+            if isinstance(child, nn.modules.instancenorm._InstanceNorm):
+                continue
+            m = nn.SyncBatchNorm.convert_sync_batchnorm(child, process_group)
+            m.weight.requires_grad_(child.weight.requires_grad)
+            m.bias.requires_grad_(child.bias.requires_grad)
+            m.to(next(child.parameters()).device)
+            setattr(model, child_name, m)
+        else:
+            convert_sync_bn(child, process_group)
+
+
+def synchronize():
+    """
+    Helper function to synchronize (barrier) among all processes when
+    using distributed training
+    """
+    if not dist.is_available():
+        return
+    if not dist.is_initialized():
+        return
+    world_size = dist.get_world_size()
+    if world_size == 1:
+        return
+    di
