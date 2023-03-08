bd76bb755f132a386e91062781988ef1e079e0b8
LaunchGemini <metabaseguy@proton.me>
2023-03-08 11:30:43 +0000

    Yeah

--- recipes/stages/_base_/data/cifar10split_bs16.py
+++ recipes/stages/_base_/data/cifar10split_bs16.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './datasets/pipelines/rcrop_hflip_resize.py'
+    './datasets/pipelines/rcrop_hflip.py'
 ]
 
 __train_pipeline = {{_base_.train_pipeline}}
@@ -10,48 +10,40 @@ __dataset_base = 'CIFAR10'
 
 data = dict(
     samples_per_gpu=16,
-    workers_per_gpu=2,
+    workers_per_gpu=4,
     num_classes=10,
     pipeline_options=dict(
-        Resize=dict(size=224),
         RandomCrop=dict(size=32, padding=4),
         Normalize=dict(
             mean=[125.307, 122.961, 113.8575],
             std=[51.5865, 50.847, 51.255]
-        ),
-        RandomResizedCrop=dict(     # for BYOL pipeline
-            size=(112, 112)
         )
     ),
     train=dict(
         type=__dataset_type,
-        data_prefix='data/torchvision/cifar10',
         base=__dataset_base,
         train=True,
+        data_prefix='data/torchvision/cifar10',
+        num_images=1000,
         pipeline=__train_pipeline,
-        num_images=5000
+        download=True
     ),
     val=dict(
         type=__dataset_type,
         base=__dataset_base,
         train=False,
         data_prefix='data/torchvision/cifar10',
+        num_images=500,
         pipeline=__test_pipeline,
-        num_images=1000
+        download=True
     ),
     test=dict(
         type=__dataset_type,
         base=__dataset_base,
         train=False,
         data_prefix='data/torchvision/cifar10',
+        num_images=1000,
         pipeline=__test_pipeline,
-        num_images=1000
-    ),
-    unlabeled=dict(
-        type=__dataset_type,
-        base=__dataset_base,
-        train=True,
-        data_prefix='data/torchvision/cifar10',
-        # num_images=1000
+        download=True
     )
 )
