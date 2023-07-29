72b3f5e85f5c3377f5f4be1b760c8c5504baf73f
LaunchGemini <metabaseguy@proton.me>
2023-07-29 22:24:41 +0000

    ASoC: wm8990: Mark the register map as dirty when powering down

---
+++ recipes/stages/_base_/models/detectors/atss.py
@@ -0,0 +1,15 @@
+_base_ = './detector.py'
+
+model = dict(
+    type='ATSS',
+    neck=dict(
+        type='FPN',
+        in_channels=[24, 32, 96, 320],
+        out_channels=64,
+        start_level=1,
+        add_extra_convs=True,
+        extra_convs_on_inputs=False,
+        num_outs=5,
+        relu_before_extra_convs=True
+    ),
+    
